ab4f5c3d8d82e608549ca3370eca65fb3bd28b94
liuXiaoHui <1406744100@qq.com>
2018-01-20 17:34:53 +0800

    improve code for homework,such as complie library

---
+++ models/research/createTfrecord.sh
@@ -0,0 +1,4 @@
+python object_detection/dataset_tools/create_furniture_tf_record.py \
+--label_map_path=/home/xiaohui/AI_homework/tensorflow_objectDetection/models/research/furnitureData/labels_items.txt \
+--data_dir=/home/xiaohui/AI_homework/tensorflow_objectDetection/models/research \
+--output_dir=/home/xiaohui/AI_homework/tensorflow_objectDetection/models/research/output
